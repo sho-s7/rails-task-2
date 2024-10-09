5189767d4fb5e19bead88c979a2df5945ef6aae5
清水翔貴 <shimizushoki@shimizushoutakanoMacBook-Air.local>
2024-10-09 13:34:36 +0900

    feat/アカウント情報、プロフィールの編集機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,31 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!  # ユーザーがログインしているか確認
+  before_action :set_user
+
   def show_account
-
   end
-  def show_profile
 
+  def show_profile
+  end
+
+  def edit_profile
+  end
+
+  def update_profile
+    if @user.update(user_params)
+      redirect_to users_show_profile_path
+    else
+      render "edit_profile"
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:avatar, :name, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show_account', to: 'users#show_account'
   get 'users/show_profile', to: 'users#show_profile'
+  get 'users/edit_profile', to: 'users#edit_profile'
+  patch 'users/edit_profile', to: 'users#update_profile'
   root 'home#index'
   get 'home/show'
   devise_for :users
